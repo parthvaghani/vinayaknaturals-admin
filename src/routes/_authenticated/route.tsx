94a0a3888c004a88f31e33d048668ab44e44e2e2
parthvaghani <vaghani98@gmail.com>
2025-09-15 11:59:21 +0530

    Login/Dashboard render issue fix

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 import { AuthenticatedLayout } from '@/components/layout/authenticated-layout'
+import { useAuthStore } from '@/stores/authStore'
 
 export const Route = createFileRoute('/_authenticated')({
+  beforeLoad: () => {
+    const { accessToken } = useAuthStore.getState().auth
+    
+    if (!accessToken) {
+      throw redirect({
+        to: '/sign-in',
+      })
+    }
+  },
   component: AuthenticatedLayout,
 })
